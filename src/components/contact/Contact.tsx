934f6d55a871ed1d0005bbbfdb9a32822f62be7b
asdfstudio <sss.joy.one@gmail.com>
2025-02-13 17:46:02 +0600

    refactor: update folder structure

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { Element } from "react-scroll";
 import { motion } from "motion/react";
 import BlurBall from "./BlurBall";
+
 type ContactInfo = {
   iconUrl: string;
   idLink: string;

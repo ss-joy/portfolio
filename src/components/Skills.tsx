bc5fd259a8d3a80f9c70ba7564837bd522bad4cc
asdfstudio <sss.joy.one@gmail.com>
2025-02-13 15:32:55 +0600

    chore: update Skills Section text color

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,9 +7,9 @@ const Skills = () => {
   return (
     <Element name="Skills">
       <div className="w-full h-screen pt-12 relative">
-        <h2 className="text-white text-5xl font-bold mt-[64px] pb-6 relative">
+        <h2 className="text-sky-600 text-5xl font-bold mt-[64px] pb-6 relative">
           Skills
-          <DraftingCompass className="absolute size-16 -top-12 left-[-64px] -rotate-45 border-2 border-white rounded-full p-2 animate-pulse" />
+          <DraftingCompass className="text-sky-600 absolute size-16 -top-12 left-[-64px] -rotate-45 border-2 border-sky-600 rounded-full p-2 animate-pulse" />
         </h2>
         <Pills />
         <span className="text-white block text-5xl opacity-70 hover:opacity-85 rotate absolute bottom-52 left-20 rotate-45">

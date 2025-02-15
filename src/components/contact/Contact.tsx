900bfbaa451eaa55bdd531f49c3f0e945a6f01bf
asdfstudio <sss.joy.one@gmail.com>
2025-02-16 03:00:55 +0600

    fix: add linkedin icon

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,14 +1,16 @@
 import { Satellite } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { JSX } from "react";
 import { Element } from "react-scroll";
 import { motion } from "motion/react";
 import BlurBall from "./BlurBall";
 import FooterGradient from "./FooterGradient";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import LinkedIn from "@/../public/images/contacts/LinkedInIcon.svg";
 
 type ContactInfo = {
-  iconUrl: string;
+  iconUrl: string | StaticImport;
   idLink: string;
   type: "link" | "gmail";
 };
@@ -30,6 +32,11 @@ const contactInfos: ContactInfo[] = [
     type: "link",
   },
   {
+    iconUrl: LinkedIn,
+    idLink: "https://www.linkedin.com/in/sakil-sazzad-joy-56a716274/",
+    type: "link",
+  },
+  {
     iconUrl: "https://cdn.simpleicons.org/gmail/ffffff",
     idLink: "sss.joy.one@gmail.com",
     type: "gmail",
@@ -38,75 +45,41 @@ const contactInfos: ContactInfo[] = [
 const Contact = () => {
   return (
     <Element name="Contact">
-      <section className="w-full h-screen pt-12 relative">
+      <section className="w-full min-h-screen pt-12 relative">
         <h2 className="text-white text-5xl font-bold mt-[64px] pb-6 relative">
           Contact Me
           <Satellite className="absolute size-16 -top-12 left-[-64px] border-2 border-white rounded-full p-2 animate-pulse" />
         </h2>
 
-        <div className="grid grid-cols-2 grid-rows-2 items-center place-items-center h-1/2 w-1/2 mx-auto relative z-20">
+        <div className="grid grid-cols-2 gap-y-4 items-center place-items-center h-1/2 w-1/2 mx-auto relative z-20">
           {contactInfos.map((cinfo, index) => {
-            if (cinfo.type === "link") {
-              return (
-                <motion.div
-                  whileHover={{
-                    scale: 0.7,
-                    transition: {
-                      type: "spring",
-                      duration: 0.6,
-                      stiffness: 100,
-                    },
-                  }}
+            return (
+              <motion.div
+                whileHover={{
+                  scale: 0.7,
+                  transition: {
+                    type: "spring",
+                    duration: 0.6,
+                    stiffness: 100,
+                  },
+                }}
+              >
+                <Link
+                  key={index}
+                  href={cinfo.idLink}
+                  target="_blank"
+                  className={`block size-[180px] hover:border-b-[10px] pb-8 border-b-white transition-all`}
                 >
-                  <Link
-                    key={index}
-                    href={cinfo.idLink}
-                    target="_blank"
-                    className={`block ${
-                      index === 0
-                        ? "size-[200px]"
-                        : "size-[130px] hover:border-b-[10px] pb-8 border-b-white transition-all"
-                    }  `}
-                  >
-                    <Image
-                      src={cinfo.iconUrl}
-                      alt="contact icon"
-                      width={100}
-                      height={100}
-                      className="block w-full h-full"
-                    />
-                  </Link>
-                </motion.div>
-              );
-            } else if (cinfo.type === "gmail") {
-              return (
-                <motion.div
-                  whileHover={{
-                    scale: 0.7,
-                    transition: {
-                      type: "spring",
-                      duration: 0.6,
-                      stiffness: 100,
-                    },
-                  }}
-                >
-                  <Link
-                    key={index}
-                    href={`mailto:${cinfo.idLink}`}
-                    target="_blank"
-                    className="block size-[200px]"
-                  >
-                    <Image
-                      src={cinfo.iconUrl}
-                      alt="contact icon"
-                      width={100}
-                      height={100}
-                      className="block w-full h-full"
-                    />
-                  </Link>
-                </motion.div>
-              );
-            }
+                  <Image
+                    src={cinfo.iconUrl}
+                    alt="contact icon"
+                    width={100}
+                    height={100}
+                    className="block w-full h-full"
+                  />
+                </Link>
+              </motion.div>
+            );
           })}
         </div>
         <BlurBall />

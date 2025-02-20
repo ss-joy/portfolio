e962a813707f688c932693bd5c85d62e91d89462
ss-joy <sss.joy.one@gmail.com>
2025-02-21 02:41:47 +0600

    fix: experience section animation bug

---
+++ src/components/experience/ExperienceYearAnimated.tsx
@@ -0,0 +1,50 @@
+import { motion } from "motion/react";
+import React, { useEffect, useState } from "react";
+
+const ExperienceYearAnimated = () => {
+  const [year, setYear] = useState(0.1);
+
+  useEffect(() => {
+    const t = setInterval(() => {
+      setYear((p) => {
+        if (p < 2) {
+          return p + 0.5;
+        }
+        return p;
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(t);
+    };
+  }, []);
+
+  return (
+    <span className="bg-white rounded-md font-bold shadow p-1 text-xl">
+      <motion.span
+        initial={{
+          scale: 0.8,
+          y: 10,
+          opacity: 0,
+        }}
+        animate={{
+          scale: [1, 1.5, 1],
+          y: [-10, 0],
+          opacity: [1, 0, 1],
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 200,
+          duration: 2,
+        }}
+        key={year}
+        className="inline-block px-1"
+      >
+        {year < 2 ? year.toFixed(1) : year.toFixed(0)}
+      </motion.span>{" "}
+      years
+    </span>
+  );
+};
+
+export default ExperienceYearAnimated;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Intro from "@/components/About";
 import Certifications from "@/components/Certifications";
 import Contact from "@/components/contact/Contact";
 import Education from "@/components/Education";
-import Experience from "@/components/Experience";
+import Experience from "@/components/experience/Experience";
 import NavBar from "@/components/NavBar";
 import { SideNavBar } from "@/components/navigations/SideNavBar";
 import Projects from "@/components/projects/Projects";
@@ -14,6 +14,7 @@ export default function Home() {
     <div className="bg-gradient-to-b from-white via-sky-300 to-sky-600 px-4">
       <div className="max-w-[1080px] mx-auto">
         <SideNavBar />
+        {/* <button className="fixed right-2 top-1/2 z-40">sei</button> */}
         <NavBar />
         <Intro />
         <Experience />

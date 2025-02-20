b2c43e6f580a8157231b0628e7c6f9f114a5605f
ss-joy <sss.joy.one@gmail.com>
2025-02-20 21:57:25 +0600

    add css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Contact from "@/components/contact/Contact";
 import Education from "@/components/Education";
 import Experience from "@/components/Experience";
 import NavBar from "@/components/NavBar";
+import { SideNavBar } from "@/components/navigations/SideNavBar";
 import Projects from "@/components/projects/Projects";
 import ScrollToTop from "@/components/ScrollToTop";
 import Skills from "@/components/Skills";
@@ -12,6 +13,7 @@ export default function Home() {
   return (
     <div className="bg-gradient-to-b from-white via-sky-300 to-sky-600 px-4">
       <div className="max-w-[1080px] mx-auto">
+        <SideNavBar />
         <NavBar />
         <Intro />
         <Experience />

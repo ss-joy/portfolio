d7f993c5f7cc8cbfdff345b9edac56bcdea41aeb
ss-joy <sss.joy.one@gmail.com>
2025-02-22 22:39:42 +0600

    chore: update animation

--- src/components/projects/ProjectIntro.tsx
+++ src/components/projects/ProjectIntro.tsx
@@ -1,31 +1,75 @@
 import React from "react";
 import GradientText from "../common/GradientText";
 import { PointerIcon } from "lucide-react";
-import Image from "next/image";
-import nodeJs from "@/../public/icons/nodejs.png";
 import { motion } from "motion/react";
 import { v4 } from "uuid";
 
-const largeSectionVariants = {
+const revealParentVariants = {
   blurred: {},
-  stable: {
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
+  stable: {},
 };
 
-const largeSectionChildVariants = {
+const revealChildVariants = {
   blurred: {
-    scale: 1.5,
-    opacity: 0.1,
+    scale: 5,
+    opacity: 0,
   },
   stable: {
     scale: 1,
     opacity: 1,
     transition: {
+      type: "tween",
+      duration: 1.8,
+    },
+  },
+};
+
+const typeWriterParentVariants = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const typeWriterChildVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+    color: "#0284c7",
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    color: "#075985",
+    transition: {
       type: "spring",
-      duration: 0.7,
+      stiffness: 300,
+    },
+  },
+};
+
+const bounceInParentVarients = {
+  bottom: {},
+  atPlace: {
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const bounceInChildVarients = {
+  bottom: {
+    y: 100,
+    opacity: 0,
+  },
+  atPlace: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 3,
+      type: "spring",
     },
   },
 };
@@ -34,131 +78,111 @@ const ProjectIntro = () => {
   return (
     <section className="grid grid-cols-auto-360 md:grid-cols-auto-500 gap-4 relative">
       <motion.div
-        variants={largeSectionVariants}
+        variants={revealParentVariants}
         initial="blurred"
         whileInView="stable"
-        id="years"
         viewport={{
           once: true,
         }}
-        transition={{
-          ease: "linear",
-        }}
-        className="md:col-span-2 border-2 bg-white/30 backdrop-blur-24 border-white/30 flex flex-col lg:flex-row items-center justify-between p-2 md:p-8 rounded-lg"
+        id="years"
+        className="md:col-span-2 border-2 bg-white/30 backdrop-blur-24 border-white/30 flex flex-col lg:flex-row items-center justify-between p-2 md:p-8 rounded-lg overflow-hidden"
       >
         <motion.div
-          variants={largeSectionChildVariants}
-          viewport={{
-            once: true,
-          }}
+          variants={revealChildVariants}
           className="text-8xl md:text-9xl bg-gradient-to-r from-sky-900 via bg-sky-500 to-sky-600 bg-clip-text text-transparent"
         >
           7+
         </motion.div>
         <motion.div
-          variants={largeSectionChildVariants}
-          viewport={{
-            once: true,
-          }}
+          variants={revealChildVariants}
           className="text-5xl bg-gradient-to-r from-sky-900 via bg-sky-500 to-sky-600 bg-clip-text text-transparent  leading-[3]"
         >
           Professional
         </motion.div>
         <motion.div
-          variants={largeSectionChildVariants}
-          viewport={{
-            once: true,
-          }}
-          className="text-3xl md:text-5xl bg-gradient-to-r from-sky-900 via bg-sky-500 to-sky-600 bg-clip-text text-transparent  leading-[3]"
+          variants={revealChildVariants}
+          className="text-3xl md:text-5xl md:leading-[3] bg-gradient-to-r from-sky-900 via bg-sky-500 to-sky-600 bg-clip-text text-transparent"
         >
           Projects Completed
         </motion.div>
       </motion.div>
+
       <div className="border-2 border-white/30 p-2 md:p-8 bg-white/30 backdrop-blur-24 rounded-lg">
         <motion.div
-          variants={largeSectionVariants}
-          initial="blurred"
-          whileInView="stable"
+          variants={typeWriterParentVariants}
           viewport={{
             once: true,
           }}
-          className="text-8xl md:text-9xl bg-gradient-to-r from-sky-900 via bg-sky-500 to-sky-600 bg-clip-text text-transparent"
+          initial="hidden"
+          whileInView="visible"
+          className="text-8xl md:text-9xl text-sky-900"
         >
           {"Full Stack".split("").map((l) => (
             <motion.span
+              variants={typeWriterChildVariants}
+              className="inline-block"
               key={v4()}
-              viewport={{
-                once: true,
-              }}
-              className=""
-              variants={largeSectionChildVariants}
             >
-              {l}
+              {l === " " ? "\u00A0" : l}
             </motion.span>
           ))}
         </motion.div>
       </div>
       <div className="border-2 border-white/30 p-2 md:p-8 bg-white/30 backdrop-blur-24 rounded-lg">
         <motion.ul
-          variants={largeSectionVariants}
-          initial="blurred"
-          whileInView="stable"
+          variants={bounceInParentVarients}
           viewport={{
             once: true,
           }}
-          transition={{
-            delay: 4,
-          }}
-          className="flex flex-col gap-4 text-slate-600 text-2xl"
+          initial="bottom"
+          whileInView="atPlace"
+          className="flex flex-col gap-4 text-sky-800 text-2xl"
         >
           <motion.li
-            variants={largeSectionChildVariants}
+            variants={bounceInChildVarients}
             className="flex items-center gap-2"
           >
             <PointerIcon className="rotate-90 stroke-sky-800" /> Covering
             Frontend
           </motion.li>
           <motion.li
-            variants={largeSectionChildVariants}
+            variants={bounceInChildVarients}
             className="flex items-center gap-2"
           >
             <PointerIcon className="rotate-90 stroke-sky-800" /> Integrating
             Backend
           </motion.li>
           <motion.li
-            variants={largeSectionChildVariants}
+            variants={bounceInChildVarients}
             className="flex items-center gap-2"
           >
             <PointerIcon className="rotate-90 stroke-sky-800" /> Databse
             Management
           </motion.li>
           <motion.li
-            variants={largeSectionChildVariants}
+            variants={bounceInChildVarients}
             className="flex items-center gap-2"
           >
             <PointerIcon className="rotate-90 stroke-sky-800" /> Figma designs
-            to Code
+            to Production Code
           </motion.li>
         </motion.ul>
       </div>
-      <div className="border-2 border-white/30 p-2 md:p-8 bg-white/30 backdrop-blur-24 rounded-lg">
+      <div className="border-2 border-white/30 p-2 md:p-8 bg-white/30 backdrop-blur-24 rounded-lg ">
         <motion.div
-          variants={largeSectionVariants}
-          initial="blurred"
-          whileInView="stable"
+          variants={typeWriterParentVariants}
           viewport={{
             once: true,
           }}
-          className="text-8xl md:text-9xl bg-gradient-to-r from-sky-900 via bg-sky-500 to-sky-600 bg-clip-text text-transparent"
+          initial="hidden"
+          whileInView="visible"
+          className="text-8xl md:text-9xl text-sky-900"
         >
           {"MERN".split("").map((l) => (
             <motion.span
+              variants={typeWriterChildVariants}
+              className="inline-block"
               key={v4()}
-              viewport={{
-                once: true,
-              }}
-              className=""
-              variants={largeSectionChildVariants}
             >
               {l}
             </motion.span>

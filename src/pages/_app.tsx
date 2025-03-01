f8647cf68c836f301f01bdba0830f1b40760fe20
ss-joy <sss.joy.one@gmail.com>
2025-03-02 05:00:35 +0600

    feat: add roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -8,7 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>SS Joy</title>
       </Head>
-      <Component {...pageProps} />
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
